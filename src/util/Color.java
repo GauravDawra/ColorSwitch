97279e96fe01a4a04951e340f546dae02880c386
Gaurav Dawra <gaurav19039@iiitd.ac.in>
2020-11-25 02:34:43 +0530

    [change] getColor() return javafx.scene.paint.Color now

--- src/util/Color.java
+++ src/util/Color.java
@@ -6,12 +6,12 @@ public enum Color {
     PINK(255,0,148),
     YELLOW(255, 255, 0);
 
-    private java.awt.Color col;
+    private javafx.scene.paint.Color col;
     Color(int r, int g, int b) {
-        this.col = new java.awt.Color(r, g, b);
+        this.col = javafx.scene.paint.Color.rgb(r, g, b);
     }
 
-    public java.awt.Color getColor() {
+    public javafx.scene.paint.Color getColor() {
         return this.col;
     }
 
