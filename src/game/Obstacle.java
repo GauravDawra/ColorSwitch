8756e1d55efc058c4bd00aa80ab6092a2bf96e7f
avgupt <avishig012@gmail.com>
2020-11-24 19:32:06 +0530

    Create Obstacle class

---
+++ src/game/Obstacle.java
@@ -0,0 +1,19 @@
+package game;
+
+public class Obstacle extends GameObject {
+    private double speed;
+    private int noOfColors;
+
+    Obstacle(double speed, int noOfColors) {
+        this.speed = speed;
+        this.noOfColors = noOfColors;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public int getNoOfColors() {
+        return noOfColors;
+    }
+}
