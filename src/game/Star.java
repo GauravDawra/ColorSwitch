fbd554eeb795e52aacde6a663df80264616c30cf
avgupt <avishig012@gmail.com>
2020-11-25 01:58:16 +0530

    Create Star class

---
+++ src/game/Star.java
@@ -0,0 +1,24 @@
+package game;
+
+import util.Removable;
+
+public class Star extends GameObject implements Removable {
+    public static final int VALUE = 1;
+    Star() {
+
+    }
+
+    public void split() {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void display() {
+
+    }
+}
