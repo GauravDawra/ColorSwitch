6cf7e6aee76faa219ccdf8297ea25593eb0904ae
avgupt <avishig012@gmail.com>
2020-11-24 18:36:33 +0530

    Add gamePage and continueGame

---
+++ src/gamePage/Controller.java
@@ -0,0 +1,15 @@
+package gamePage;
+
+import javafx.fxml.Initializable;
+import javafx.scene.shape.Arc;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
