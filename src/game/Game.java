df5d9d64c5116fb81ce5347e91bff2c16c8f8f85
Gaurav Dawra <gaurav19039@iiitd.ac.in>
2020-11-24 19:16:26 +0530

    [add] Game.java

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,7 +1,56 @@
 package game;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Game implements Serializable {
+    enum State {
+        PLAYING,
+        PAUSED,
+        EXIT
+    }
 
+//    private Ball ball;
+    public static final double gravity = 5.0;
+    private int score;
+    private State state;
+//    private ArrayList<GameObject> component;
+    private Date date;
+
+    public Game() {
+        this.score = 0;
+        this.state = State.PLAYING;
+        this.date = java.util.Calendar.getInstance().getTime();
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void setScore(int sc) {
+        if(sc >= 0 && sc > getScore())
+            this.score = sc;
+    }
+
+    public State getState() {
+        return this.state;
+    }
+
+    public void setState(State s) {
+        this.state = s;
+    }
+
+    public void play() {
+        setState(State.PLAYING);
+        // ...
+    }
+
+    public void pause() {
+        setState(State.PAUSED);
+    }
+
+    public void exit() {
+        setState(State.EXIT);
+    }
 }
