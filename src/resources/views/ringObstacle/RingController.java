3c73c3f5d89a405934b70cd57865855466920f99
avgupt <avishig012@gmail.com>
2020-11-28 21:06:17 +0530

    Add rotation animation

--- src/resources/views/ringObstacle/RingController.java
+++ src/resources/views/ringObstacle/RingController.java
@@ -1,4 +1,4 @@
-package RingObstacle;
+package resources.views.ringObstacle;
 
 import javafx.animation.Animation;
 import javafx.animation.Interpolator;
@@ -17,61 +17,16 @@ import java.util.ResourceBundle;
 
 public class RingController implements Initializable {
     @FXML
-    public AnchorPane innerCircle2;
-    @FXML
-    public AnchorPane outerCircle;
-    @FXML
-    public AnchorPane outerMostCircle;
-    @FXML
-    public ImageView playButton;
+    public AnchorPane ringObstacle;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-//        System.out.println("hello");
-        RotateTransition rotate = new RotateTransition(Duration.millis(2000), innerCircle2);
+        RotateTransition rotate = new RotateTransition(Duration.millis(2000), ringObstacle);
         rotate.setByAngle(360);
         rotate.setCycleCount(Animation.INDEFINITE);
         rotate.setInterpolator(Interpolator.LINEAR);
         rotate.play();
 
-
-        RotateTransition rotateOuter = new RotateTransition(Duration.millis(3500), outerCircle);
-        rotateOuter.setByAngle(-360);
-        rotateOuter.setCycleCount(Animation.INDEFINITE);
-        rotateOuter.setInterpolator(Interpolator.LINEAR);
-        rotateOuter.play();
-
-        RotateTransition rotateOuterMost = new RotateTransition(Duration.millis(2800), outerMostCircle);
-        rotateOuterMost.setByAngle(360);
-        rotateOuterMost.setCycleCount(Animation.INDEFINITE);
-        rotateOuterMost.setInterpolator(Interpolator.LINEAR);
-        rotateOuterMost.play();
-
-        scaleAnimation(playButton, 0.25f);
-
-    }
-
-    private void scaleAnimation(ImageView image, double val) {
-        ScaleTransition scalePlay = new ScaleTransition(Duration.millis(750), image);
-        scalePlay.setByX(val);
-        scalePlay.setByY(val);
-        scalePlay.setCycleCount(Animation.INDEFINITE);
-        scalePlay.setInterpolator(Interpolator.EASE_BOTH);
-        scalePlay.setAutoReverse(true);
-        scalePlay.play();
-    }
-
-    public void keyPressed(KeyEvent keyEvent) {
-        System.out.println("hello");
-    }
-
-    public void hello(KeyEvent keyEvent) {
-        System.out.println("hello");
-        System.out.println(keyEvent.getSource().toString());
-    }
-
-    public void playPressed(MouseEvent mouseEvent) {
-        System.out.println("play pressed");
     }
 
 }
