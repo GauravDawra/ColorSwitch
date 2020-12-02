7acdc36a8697ab3253849f0cb29d71ca4c9c01e0
avgupt <avishig012@gmail.com>
2020-12-02 15:20:46 +0530

    Add relative motion of obstacles to ball

--- src/game/Game.java
+++ src/game/Game.java
@@ -99,21 +99,43 @@ public class Game extends Application implements Serializable {
     @Override
     public void start(Stage primaryStage) throws Exception {
         /* All this code is just for testing right now*/
+
+        primaryStage.setTitle("Color Switch");
+        primaryStage.setHeight(HEIGHT);
+        primaryStage.setWidth(WIDTH);
         primaryStage.setResizable(false);
-        AnchorPane sp = new AnchorPane();
-        sp.setStyle("-fx-background-color: black;");
-        sp.getChildren().add(ball.getNode());
-        System.out.println(ball.getNode().getClass());
-        Scene sc = new Scene(sp, WIDTH, HEIGHT);
-        primaryStage.setScene(sc);
+
+        Group root = new Group();
+
+        AnchorPane background = new AnchorPane();
+        AnchorPane obstacles = new AnchorPane();
+        AnchorPane player = new AnchorPane();
+
+        background.setPrefSize(WIDTH, HEIGHT);
+        background.setStyle("-fx-background-color: black;");
+
         ColorPalette pallete = new ColorPalette(WIDTH / 2, 500);
-        MediumRingObstacle ring = new MediumRingObstacle(WIDTH / 2, 400);
-        sp.getChildren().addAll( ring.getNode());
+        //background.getChildren().add(pallete.getNode());
+        MediumRingObstacle ring = new MediumRingObstacle(WIDTH / 2, 500);
+        MediumRingObstacle ring1 = new MediumRingObstacle(WIDTH / 2, 200);
+        obstacles.getChildren().addAll(ring.getNode(), ring1.getNode());
+
+        player.getChildren().add(ball.getNode());
+        System.out.println(ball.getNode().getClass());
+
+        root.getChildren().add(background);
+        root.getChildren().add(obstacles);
+        root.getChildren().add(player);
+
+        Scene sc = new Scene(root);
+        primaryStage.setScene(sc);
+
         sc.setOnKeyPressed(event -> {
             ball.jump();
 //                System.out.println("hello");
         });
         ring.bindToBall(ball);
+        ring1.bindToBall(ball);
 
 //        ball.getNode().centerY
 
@@ -136,6 +158,10 @@ public class Game extends Application implements Serializable {
                 }
                 if(ring.check(ball)) {
                     System.out.println("obstacle collision");
+                }
+                
+                if (ball.getPosition().getY() < 400) {
+                    obstacles.setTranslateY(400 - ball.getPosition().getY());
                 }
 //                ball.setPosition();
 //                vel.setY(vel.getY() + 9.8/5.0);
