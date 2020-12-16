928a1124f8a7ba746292a16e937b336f8bb4b6f5
avgupt <avishig012@gmail.com>
2020-12-16 14:42:39 +0530

    Change scene from main page to game page

---
+++ src/sceneLoader/SceneLoader.java
@@ -0,0 +1,34 @@
+package sceneLoader;
+
+import application.App;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
+public class SceneLoader {
+    private static SceneLoader loader = null;
+
+    private Scene gameOverPage;
+    private Scene gamePage;
+    private Scene mainPage;
+    private Scene pausePage;
+    private Scene savedGamesPage;
+
+    private SceneLoader() throws IOException {
+        Parent mainPageParent = FXMLLoader.load(getClass().getResource("/mainPage/layout.fxml"));
+        mainPage = new Scene(mainPageParent);
+    }
+
+    public static SceneLoader getLoader() throws IOException{
+        if (loader == null) {
+            loader = new SceneLoader();
+        }
+        return loader;
+    }
+
+    public Scene getMainPage() {
+        return mainPage;
+    }
+}
